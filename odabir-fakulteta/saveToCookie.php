162ac060fb6458e8229ee7ca5fb57a125ad445a9
DavidZagdal <85765065+DavidZagdal@users.noreply.github.com>
2024-05-10 13:05:57 +0200

    Update saveToCookie.php
    maknut API key za github dok se ne spremi externalno

--- odabir-fakulteta/saveToCookie.php
+++ odabir-fakulteta/saveToCookie.php
@@ -93,8 +93,8 @@
 
 
     function searchGoogleFirstPageSnippet($whatToSearch) {
-        $apiKey = 'AIzaSyApO6ij2qDPKa7UFyXKxwAwSSt8t5qGyS0';
-        $searchEngineId = 'f607880da84754171';
+        $apiKey = ''; //maknuto za public git
+        $searchEngineId = ''; // maknuto za public git
     
         $encoded = urlencode($whatToSearch);
         
@@ -129,8 +129,8 @@
     
 
     function searchGoogleFirstPage($whatToSearch) {
-        $apiKey = 'AIzaSyApO6ij2qDPKa7UFyXKxwAwSSt8t5qGyS0';
-        $searchEngineId = 'f607880da84754171';
+        $apiKey = '';
+        $searchEngineId = '';
 
         $encoded = urlencode($whatToSearch);
         

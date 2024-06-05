0841e59a4e69f471b46a99bf1240469856019e9e
david <david.gulic.ets@gmail.com>
2024-06-05 17:17:00 +0200

    gotova pitanja korisnika

--- errors/pitanja-view.php
+++ errors/pitanja-view.php
@@ -28,20 +28,22 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KudSad</title>
+    <script src="https://kit.fontawesome.com/90b9bb8c8d.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="../image/png" href="../images/favicon-32x32.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <link rel="stylesheet" href="../main-css-for-pages.css">
-    <style>
-        .container {
-            margin-top: 50px;
-        }
-        .card {
-            margin-bottom: 20px;
-        }
-    </style>
 </head>
 <body>
+
+    <div id="toolbarContainer">
+        <?php
+            require '../toolbar/whatToolbarToUse.php';
+            echo whatToolbarToUse();
+        ?>
+    </div>
+
     <div class="container">
         <h1 class="text-center">Pitanja korisnika</h1>
         <div id="questionsContainer">
